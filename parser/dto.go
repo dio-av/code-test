7ee7df05af1aed6652ed684892423e42dd89004e
Diogo Assereuy <dio.vasc9537@gmail.com>
2023-06-09 12:56:55 -0300

    improving code structure

---
+++ parser/dto.go
@@ -0,0 +1,42 @@
+package parser
+
+import "time"
+
+type PlanData struct {
+	Device struct {
+		Name string `json:"name"`
+	} `json:"device"`
+	Plans []struct {
+		ID               int     `json:"id"`
+		Type             string  `json:"type"`
+		Name             string  `json:"name"`
+		PhonePrice       int     `json:"phonePrice"`
+		PhonePriceOnPlan int     `json:"phonePriceOnPlan"`
+		Installments     int     `json:"installments"`
+		MonthlyFee       float32 `json:"monthlyFee"`
+		Schedule         struct {
+			StartDate time.Time `json:"startDate"`
+		} `json:"schedule"`
+		Region struct {
+			Name     string `json:"name"`
+			Priority int    `json:"priority"`
+		} `json:"region"`
+	} `json:"plans"`
+}
+
+type Plan struct {
+	ID               int
+	Type             string
+	Name             string
+	PhonePrice       int
+	PhonePriceOnPlan int
+	Installments     int
+	MonthlyFee       float32
+	Schedule         struct {
+		StartDate time.Time
+	}
+	Region struct {
+		Name     string
+		Priority int
+	}
+}
